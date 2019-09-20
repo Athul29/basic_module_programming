54840807b4f13415590e5299ab398f10c030562a
user_2 <user_2@FACE-29>
2019-09-20 07:05:56 -0700

    number

--- basicmodule_java/src/number/Number.java
+++ basicmodule_java/src/number/Number.java
@@ -6,89 +6,81 @@ import java.io.InputStreamReader;
 
 public class Number {
 
-public static void main(String[] args) throws NumberFormatException, IOException
-{
-int n,count=0;
-Scanner sc=new Scanner(System.in);
+public static void main(String[] args) throws IOException {
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 System.out.println("Enter the number ");
-n=sc.nextInt();
-while(n!=0)
+int number=Integer.parseInt(br.readLine());
+int t=0;
+int n=number/10;
+int r=number%10;
+if((number>=20)||(number<=10))
 {
-if(n<10)
-{
-switch(n)
+
+
+
+switch(r)
 {
 case 1:
 case 2:
-case 10:
-case 6:count=count+3;break;
-//System.out.println("3");break;
-case 0:
+case 6:
+t=t+3;
+break;
 case 4:
+case 5:
 case 9:
-case 5:count=count+4;break;//System.out.println("4");break;
+t=t+4;
+break;
 case 3:
+case 8:
 case 7:
-case 8:count=count+5;break;//System.out.println("5");break;
+t=t+5;
+break;
+
+}
+
+switch(n)
+{
+case 3:
+case 2:
+case 8:
+case 9:
+t=t+6;
+break;
+case 4:
+case 5:
+case 6:
+t=t+5;
+break;
+case 7:
+t=t+7;
+break;
+
+
+}
+}
+else
+{
+switch(number)
+{
 case 11:
-case 12:count=count+6;break;//System.out.println("6");break;
-case 15:
-case 16:count=count+7;break;//System.out.println("7");break;
+case 12:
+t=t+6;
+break;
+case 16:
+t=t+7;
+break;
 case 13:
+case 14:
 case 18:
-case 19:count=count+8;break;//System.out.println("8");break;
+case 19:
+t=t+8;
+break;
 case 17:
-case 14:count=count+9;break;
+t=t+9;
+break;
 }
 }
-if(n>=20 && n<=99)
-{
-int r=(n/10)*10;
-System.out.println(r);
-switch(r)
-{
-case 20:
-case 30:
-case 80:
-count=count+6;break;
-case 40:
-case 50:
-case 90:
-case 60:count=count+5;break;
-case 70:count=count+7;break;
-
-
-}
-
-
-}
-/* if(n>=20 && n<=90)
-{
-int r=(n/10)*10;
-System.out.println(r);
-switch(r)
-{
-case 20:
-case 30:
-case 80:
-case 90:count=count+6;break;
-case 40:
-case 50:
-case 60:count=count+5;break;
-case 70:count=count+7;break;
-
-
-}
-//n=n/10;
-
-}*/
-n=n/10;
-
-}
-
-System.out.println("count= "+count);
-
+System.out.println(t);
 }
 
 }
-
